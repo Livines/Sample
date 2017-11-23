3c2c56378444f78cdcedbd8a04a797bd0523bd1e
Livin <livines068@gmail.com>
2017-11-23 14:33:09 +0530

    New Class added

--- app/src/main/java/career/com/test4/Final.java
+++ app/src/main/java/career/com/test4/Final.java
@@ -4,16 +4,12 @@ import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import java.util.List;
-
-/**
- * Created by Ramees on 11/23/2017.
- */
 
 public class Final extends AppCompatActivity {
     private static final String urlday = "http://mausam.co";
     private String keyword;
     private ProgressDialog progressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/career/com/test4/Last.java
+++ app/src/main/java/career/com/test4/Last.java
@@ -3,9 +3,7 @@ package career.com.test4;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-/**
- * Created by Ramees on 11/23/2017.
- */
+
 
 public class Last extends AppCompatActivity {
 
